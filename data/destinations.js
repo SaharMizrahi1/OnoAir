3b6ba484979d302e819c07f7e68051ea1dbe8304
shaked yael <shakedy10@gmail.com>
2024-11-26 18:41:15 +0200

    instead of json , create 10 objects - still missing the bookings

---
+++ data/destinations.js
@@ -0,0 +1,52 @@
+export const destinations = [
+    {
+        name: "Tel Aviv",
+        airportName: "Ben Gurion Airport",
+        airportUrl: "https://www.iaa.gov.il/en/"
+    },
+    {
+        name: "New York",
+        airportName: "John F. Kennedy International Airport",
+        airportUrl: "https://www.jfkairport.com/"
+    },
+    {
+        name: "London",
+        airportName: "Heathrow Airport",
+        airportUrl: "https://www.heathrow.com/"
+    },
+    {
+        name: "Paris",
+        airportName: "Charles de Gaulle Airport",
+        airportUrl: "https://www.parisaeroport.fr/"
+    },
+    {
+        name: "Berlin",
+        airportName: "Berlin Brandenburg Airport",
+        airportUrl: "https://ber.berlin-airport.de/"
+    },
+    {
+        name: "Tokyo",
+        airportName: "Narita International Airport",
+        airportUrl: "https://www.narita-airport.jp/en/"
+    },
+    {
+        name: "Sydney",
+        airportName: "Sydney Kingsford Smith Airport",
+        airportUrl: "https://www.sydneyairport.com.au/"
+    },
+    {
+        name: "Dubai",
+        airportName: "Dubai International Airport",
+        airportUrl: "https://www.dubaiairports.ae/"
+    },
+    {
+        name: "Los Angeles",
+        airportName: "LAX Airport",
+        airportUrl: "https://www.flylax.com/"
+    },
+    {
+        name: "Singapore",
+        airportName: "Changi Airport",
+        airportUrl: "https://www.changiairport.com/"
+    }
+];
---
+++ data/flights.js
@@ -0,0 +1,33 @@
+export const flights = [
+    {
+        flightNo: "W61283",
+        origin: "Tel Aviv",
+        destination: "Krakow",
+        departureDate: "2024-12-25",
+        departureTime: "10:00",
+        arrivalDate: "2024-12-25",
+        arrivalTime: "14:00",
+        seats: 180
+    },
+    {
+        flightNo: "LX8396",
+        origin: "Zurich",
+        destination: "Larnaca",
+        departureDate: "2024-12-30",
+        departureTime: "08:00",
+        arrivalDate: "2024-12-30",
+        arrivalTime: "12:00",
+        seats: 150
+    },
+    {
+        flightNo: "BA456",
+        origin: "London",
+        destination: "New York",
+        departureDate: "2024-11-20",
+        departureTime: "09:30",
+        arrivalDate: "2024-11-20",
+        arrivalTime: "14:30",
+        seats: 300
+    },
+    
+];
